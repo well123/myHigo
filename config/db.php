6582ec297476e0bcc97bf6325fd46346965c2d5a
well123 <940962556@qq.com>
2016-06-25 17:22:16 +0800

    ds

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => 'well123!',
+    'charset' => 'utf8',
+];
