86d723d0b91bce3f867678425f379611ba7c2dad
hzj <510048138@qq.com>
2016-07-10 23:57:41 +0800

    框架

--- service/HigoClient.php
+++ service/HigoClient.php
@@ -7,13 +7,14 @@
  */
 namespace app\service;
 
-class HigoClient{
+use app\service\MyFunction;
+class HigoClient {
 
     private static $loginUrl = '';
     private static $logoutUrl = '';
     private static $userInfoUrl = '';
 
-    public static function login(){
+    public static function login() {
         /**
          * $userInfo = array('name'=>'','password'=>'','caption'=>'');
          * 需要你来写获取用户登录名和密码的方法
@@ -23,7 +24,64 @@ class HigoClient{
         return self::isLoginSuccess($response);
     }
 
-    private static function isLoginSuccess(Array $array){
+    private static function isLoginSuccess(Array $array) {
         return $array['code'] == 0;
+    }
+
+    /**
+     * 拼接url
+     * @param $ip string  ip地址
+     * @param $type string url类型
+     */
+    public static function setUrl($ip, $type) {
+
+    }
+
+    /**
+     * 左侧信息请求
+     */
+    public static function leftInfo() {
+        $response = HttpClient::curl(self::$loginUrl);
+        return self::isGetLeftInfoSuccess($response);
+    }
+
+    /**
+     * 获取左侧数据
+     * @param $string
+     * @return bool
+     */
+    private static function isGetLeftInfoSuccess($string) {
+        $userName = MyFunction::InterceptString($string, '{"account":"', '","credit"');    //用户名
+        $edu = MyFunction::InterceptString($string, ',"credit":"', '","re_credit"');    //信用额度
+        $yue = MyFunction::InterceptString($string, ',"total_amount":"', '","odds_refresh"');    //信用余额
+    }
+
+    /**
+     * 时时彩页面信息请求
+     * @param $data array post数据
+     * @return bool
+     */
+    public static function sscInfo($data = array()) {
+        if (empty($data)) {
+            $data = array('action' => 'ajax');
+        }
+        $response = HttpClient::curl(self::$loginUrl, $data);
+        return self::isGetSscInfo($response);
+    }
+
+    /**
+     * 获取上期开奖结果，本期，赔率
+     * @param $string
+     */
+    private static function isGetSscInfo($string){
+        $old = MyFunction::InterceptString($string);
+    }
+
+    /**
+     * 购买
+     * @param sring购买
+     */
+    public static function buy($data=''){
+
     }
 }
---
+++ service/MyFunction.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\service;
+
+class MyFunction{
+    /**
+     * 截取字符串
+     */
+    public static function InterceptString($string,$s,$e){
+        $a = strpos($string,$s);
+        $b = strpos($string,$e);
+        return substr($string,$a+strlen($s),$b-$a-strlen($s));
+    }
+}
