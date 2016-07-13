dd23fd3d2769bda31e4c83d69d2733a29c1544e5
well123 <940962556@qq.com>
2016-07-13 19:11:53 +0800

    修改表格

--- controllers/DemoController.php
+++ controllers/DemoController.php
@@ -10,12 +10,12 @@ class DemoController extends Controller{
     public function actionIndex(){
 //        HttpClient::curl('http://sms.huhutv.com.cn/rtcrm-clientweb/npage/obim/staff/loginmng/initLogin.do');
 //        HttpClient::downLoadCaptcha('http://sms.huhutv.com.cn/rtcrm-clientweb/npage/obim/staff/loginmng/queryRandomImage.do');
-        var_dump(HttpClient::curl('http://sms.huhutv.com.cn/rtcrm-clientweb/npage/obim/staff/loginmng/getValidSystemLogin.do',array(
-            'systemUserCode'=>'A-08',
-            'password'=>'Bb654321',
-            'valid'=>'N',
-            'randomWord'=>'8jkh'
-        )));
+//        var_dump(HttpClient::curl('http://sms.huhutv.com.cn/rtcrm-clientweb/npage/obim/staff/loginmng/getValidSystemLogin.do',array(
+//            'systemUserCode'=>'A-08',
+//            'password'=>'Bb654321',
+//            'valid'=>'N',
+//            'randomWord'=>'8jkh'
+//        )));
         exit;
     }
 }
