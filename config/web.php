39ff7d961176d1ec1b6a794cdbc172d636df56f8
hzj <510048138@qq.com>
2016-07-15 22:04:18 +0800

    测试次改

--- config/web.php
+++ config/web.php
@@ -38,14 +38,7 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
+
     ],
     'params' => $params,
     'message' => $message,
